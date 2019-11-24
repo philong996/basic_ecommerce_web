35721a4b39ac8ba1847f51f7bdef7ca79e5a4389
philong996 <tenbox1996@gmail.com>
2019-11-24 21:56:00 +0700

    commit everything

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for
+import psycopg2
+import requests
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+    categories = {}
+    conn = psycopg2.connect(user = 'philong', database = 'tiki', password = 'phan1996')
+    cur = conn.cursor()
+    query ='''
+    SELECT 
+        name 
+    FROM categories
+    WHERE parent_id is null;
+    '''
+    cur.execute(query)
+    names = cur.fetchall()
+    img_links = [
+        'https://image.flaticon.com/icons/svg/1079/1079539.svg',
+        'https://image.flaticon.com/icons/svg/1008/1008286.svg',
+        'https://image.flaticon.com/icons/svg/1298/1298457.svg',
+        'https://image.flaticon.com/icons/svg/1299/1299833.svg',
+        'https://image.flaticon.com/icons/svg/1299/1299837.svg',
+        'https://image.flaticon.com/icons/svg/1135/1135249.svg',
+        'https://image.flaticon.com/icons/svg/1178/1178598.svg',
+        'https://www.flaticon.com/premium-icon/icons/svg/1756/1756551.svg',
+        'https://image.flaticon.com/icons/svg/1073/1073349.svg',
+        'https://image.flaticon.com/icons/svg/1008/1008274.svg',
+        'https://image.flaticon.com/icons/svg/1008/1008279.svg',
+        'https://www.flaticon.com/premium-icon/icons/svg/1058/1058746.svg',
+        'https://image.flaticon.com/icons/svg/1288/1288128.svg',
+        'https://www.flaticon.com/premium-icon/icons/svg/1072/1072337.svg',
+        'https://image.flaticon.com/icons/svg/1080/1080330.svg',
+        'https://image.flaticon.com/icons/svg/1073/1073176.svg'
+    ]
+    cat_names = ['Điện Thoại - Máy Tính Bảng',
+            'Điện Tử - Điện Lạnh',
+            'Thiết Bị Số - Phụ Kiện Số',
+            'Laptop - Máy Vi Tính - Linh kiện',
+            'Máy Ảnh - Máy Quay Phim',
+            'Điện Gia Dụng',
+            'Nhà Cửa - Đời Sống',
+            'Bách Hóa Online',
+            'Đồ Chơi - Mẹ & Bé',
+            'Làm Đẹp - Sức Khỏe',
+            'Thời Trang',
+            'Thể Thao - Dã Ngoại',
+            'Ô Tô - Xe Máy - Xe Đạp',
+            'Hàng Quốc Tế',
+            'Nhà Sách Tiki',
+            'Voucher - Dịch vụ']
+    for i in range(len(names)):
+        categories[names[i]] = (img_links[i],cat_names[i])
+    return render_template('index.html', categories= categories)
+
+@app.route('/products/<category>/<offset>')
+def products(category,offset):
+    conn = psycopg2.connect(user = 'philong', database = 'tiki', password = 'phan1996')
+    cur = conn.cursor()
+    query = "SELECT * FROM products WHERE category = %s LIMIT 48 OFFSET %s"
+    val = (category,offset,)
+    cur.execute(query, val)
+    products = cur.fetchall()
+
+    return render_template('products.html',products = products, category=category, offset=offset)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
---
+++ scrawl_tiki.py
@@ -0,0 +1,120 @@
+from concurrent.futures import ThreadPoolExecutor
+import psycopg2
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+
+class Product:
+    def __init__(self, productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink):
+        self.productid  = productid
+        self.images = images
+        self.fprice = fprice
+        self.category = category
+        self.subcategory = subcategory
+        self.category_id = category_id
+        self.titles = titles
+        self.seller = seller
+        self.rprice = rprice
+        self.discount = discount
+        self.ratings = ratings
+        self.num_reviews = num_reviews
+        self.tikinow = tikinow
+        self.productlink = productlink
+    def insert_product(self):
+        tablename = 'products'
+        query = f"""INSERT INTO {tablename}(productid ,images, fprice, category, subcategory, category_id , titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink)
+                    VALUES('{self.productid}', '{self.images}','{self.fprice}', '{self.category}', '{self.subcategory}','{self.category_id}' ,'{self.titles}','{self.seller}','{self.rprice}','{self.discount}','{self.ratings}','{self.num_reviews}','{self.tikinow}','{self.productlink}');"""
+        cur.execute(query)
+        conn.commit()
+    def __repr__(self):
+        return f'Category: {self.category}, \n Name: {self.titles}, Price: {self.fprice}'
+
+def load_website(url):
+    try:
+        response = requests.get(url)
+        return BeautifulSoup(response.text, "html.parser")
+    except Exception as err:
+        print(f'ERROR: {err}')
+
+def scrape_and_insert(cat_link,articles,k):
+    #Works for scraping beautiful soup product item from tiki product page
+    try:
+        #scrape and assign to variables
+        images = articles[k].img['src']
+        productid = articles[k]['data-id']
+        fprice = int(articles[k].find_all("span",{"class":"final-price"})[0].text.strip().split()[0].strip('đ').replace('.',''))
+        rprice = [0 if articles[k].find_all("span",{"class":"price-regular"})[0].text == '' else int(articles[k].find_all("span",{"class":"price-regular"})[0].text.strip('đ').replace('.',''))][0]
+        discount = [0 if len(articles[k].find_all("span",{"class":"final-price"})[0].text.strip().split()) == 1 else int(articles[k].find_all("span",{"class":"final-price"})[0].text.strip().split()[1].split('%')[0])][0]
+        seller = articles[k]['data-brand'].replace('\'','').replace('"','')
+        titles = articles[k].a['title'].strip().replace('\'','').replace('"','')
+        subcategory = articles[k]['data-category'].strip().split('/')[-1]
+        category = articles[k]['data-category'].strip().split('/')[0]
+        if articles[k].find_all('p',{"class":'review'}) == [] or articles[k].find_all('p',{"class":'review'})[0].text == 'Chưa có nhận xét':
+            num_reviews = 0
+        else:
+            num_reviews = int(articles[k].find_all('p',{"class":'review'})[0].text.strip('\(\)').split()[0])
+
+        ratings = [int(articles[k].find_all('span',{"class":'rating-content'})[0].find('span')['style'].split(':')[1].split('%')[0]) if articles[k].find_all('span',{"class":'rating-content'}) != [] else 0][0]
+        tikinow = [0 if articles[k].find_all('i',{"class":"tikicon icon-tikinow-20"}) == [] else 1][0]
+        productlink = articles[k].a['href']
+        category_id = cat_link[0]
+        return (productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink)
+        
+    except Exception as err:
+        print(err, k)
+
+def get_products(cat_link):
+    t1 = time.perf_counter()
+    count = 0
+    soup = load_website(cat_link[1])
+    links = soup.find('a',{"class":'next'})
+    if links == None:
+        articles = soup.find_all('div', {"class":'product-item'})
+        for k in range(len(articles)):
+            productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink = scrape_and_insert(cat_link,articles, k)
+            product = Product( productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink)
+
+            #print(product)
+            count += 1
+            product.insert_product()
+    else:
+        links = soup.find('a',{"class":'next'})
+        while links != None:
+            articles = soup.find_all('div', {"class":"product-item"})
+            soup = load_website(('https://tiki.vn' + links['href']))
+            for k in range(len(articles)):
+                productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink = scrape_and_insert(cat_link,articles, k)
+                product = Product( productid, images, fprice, category, subcategory, category_id, titles, seller, rprice, discount, ratings, num_reviews, tikinow, productlink)
+                product.insert_product()
+                #print(product)
+                count += 1
+            links = soup.find('a',{"class":'next'})
+    t2 = time.perf_counter()
+    duration = f'Duration Process: {t2 - t1}'
+
+    print(cat_link[1], count , duration)
+
+
+query = '''SELECT p.id, p.url FROM categories as p
+    LEFT JOIN categories as c ON c.parent_id = p.id
+    WHERE c.id IS NULL;
+'''
+
+conn = psycopg2.connect(user="philong",
+                        password="phan1996",
+                        database="tiki")
+conn.autocommit = True
+cur = conn.cursor()
+
+cur.execute(query)
+urls = cur.fetchall()
+
+
+start = time.perf_counter()
+with ThreadPoolExecutor() as executor:
+    executor.map(get_products, urls)
+
+end = time.perf_counter()
+print(f'Time for Process: {end - start}')
